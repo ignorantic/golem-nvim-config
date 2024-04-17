eb3fb498532c536438e25fa51354e34d5cb45925
Andrii Sorokin <ansolaris@gmail.com>
2024-04-18 00:01:40 +0300

    Add: lualine sections config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,8 +1,50 @@
+local function isNvimTree()
+  return vim.bo.filetype == "NvimTree"
+end
+
+local function isFile()
+  return vim.bo.filetype ~= "NvimTree" and vim.bo.filetype ~= "packer"
+end
+
 require("lualine").setup({
   options = {
     icons_enabled = true,
     theme = "gruvbox-material",
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = ''},
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+  },
+  sections = {
+    lualine_a = {
+      { "mode", cond = isFile }
+    },
+    lualine_b = {
+      { "branch", cond = isFile },
+      { "diff", cond = isFile },
+      { "diagnostics", cond = isFile }
+    },
+    lualine_c = {
+      { "filename", cond = isFile },
+      { "progress", cond = isNvimTree }
+    },
+    lualine_x = {
+      { "encoding", cond = isFile },
+      { "fileformat", cond = isFile },
+      { "filetype", cond = isFile },
+      { "location", cond = isNvimTree }
+    },
+    lualine_y = {
+      { "progress", cond = isFile }
+    },
+    lualine_z = {
+      { "location", cond = isFile }
+    }
+  },
+  inactive_sections = {
+    lualine_c = {
+      { "filename", cond = isFile }
+    },
+    lualine_x = {
+      { "location", cond = isFile }
+    }
   }
 })
